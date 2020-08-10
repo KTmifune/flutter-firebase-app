18249dd8153608aed08e37b7d5f069108f87ef7c
Takuma Kato <k.t.mifune@gmail.com>
2020-08-10 23:50:12 +0900

    no message

---
+++ firebase_sample/android/app/src/main/kotlin/com/katotama/firebase_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.katotama.firebase_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ firebase_sample/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_sample/app/sign_in/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Time Ticker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignInPage(),
+    );
+  }
+}
