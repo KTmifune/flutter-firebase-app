25927d7525bc89c1a16abc7f4e1187a8c653485d
Takuma Kato <k.t.mifune@gmail.com>
2020-08-30 23:13:55 +0900

    no message

--- firebase_sample/lib/main.dart
+++ firebase_sample/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Time',
+      title: 'Camera App',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         visualDensity: VisualDensity.adaptivePlatformDensity,
