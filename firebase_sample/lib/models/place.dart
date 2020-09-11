494f8af1c05db1c3f297adce6551e2328a679bfc
Takuma Kato <k.t.mifune@gmail.com>
2020-09-11 23:53:56 +0900

    no message

--- firebase_sample/lib/models/place.dart
+++ firebase_sample/lib/models/place.dart
@@ -2,17 +2,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'dart:io';
 
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  PlaceLocation({
+    @required this.latitude,
+    @required this.longitude,
+    this.address,
+  });
+}
+
 class Place {
   final String id;
   final String title;
-  final ... image;
-  final ... location;
+  final File image;
+  final PlaceLocation location;
 
   Place({
-  @required this.id,
-  @required this.title,
-  @required this.image,
-  @required this.location,
+    @required this.id,
+    @required this.title,
+    @required this.image,
+    @required this.location,
   });
-
 }
